fa66d403657de194221a6b85cc1cd23024861ac6
sk <liyulong@yuelvhui.com>
2020-05-12 11:15:29 +0800

    Initial Commit

---
+++ StaticVC/StaticVC.h
@@ -0,0 +1,19 @@
+//
+//  StaticVC.h
+//  StaticVC
+//
+//  Created by 李雨龙 on 2020/5/12.
+//  Copyright © 2020 李雨龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StaticVC.
+FOUNDATION_EXPORT double StaticVCVersionNumber;
+
+//! Project version string for StaticVC.
+FOUNDATION_EXPORT const unsigned char StaticVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticVC/PublicHeader.h>
+
+
